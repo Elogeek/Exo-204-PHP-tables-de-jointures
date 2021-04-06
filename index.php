84e6912c02cba18af393f7d2ffad32befb8e22c7
elogeek <elodiechristin@gmail.com>
2021-04-06 19:37:10 +0200

    go

--- index.php
+++ index.php
@@ -7,3 +7,16 @@
  * 4 - A l'aide d'un simple print_r, afficher les rôles de chaque utilisateur.
  * 5 - FIN !
  */
+
+    require_once "DB/DB.php";
+
+$sql = DB::getInstance()->prepare("SELECT * FROM user_role INNER JOIN user ON user.id = user_role.user_fk INNER JOIN role ON role.id = user_role.role_fk");
+$result = $sql->execute();
+
+if($result) {
+    foreach ($sql->fetchAll() as $value) {
+        echo "<pre>";
+        print_r($value);
+        echo "</pre>";
+    }
+}
